dc9075ccc90fa62a4eee1a7c984ff5ab4b9c25c2
Deadlica <samuelgreenberg00@gmail.com>
2024-12-15 21:09:54 +0100

    exercise 1 done

--- Assignment IV/ex_1/main.cu
+++ Assignment IV/ex_1/main.cu
@@ -1,5 +1,24 @@
 #include <iostream>
 
+__global__ void PictureKernel(float* d_Pin, float* d_Pout, int n, int m) {
+
+    // Calculate the row # of the d_Pin and d_Pout element to process
+
+    int Row = blockIdx.y*blockDim.y + threadIdx.y;
+
+    // Calculate the column # of the d_Pin and d_Pout element to process
+
+    int Col = blockIdx.x*blockDim.x + threadIdx.x;
+
+    // each thread computes one element of d_Pout if in range
+
+    if ((Row < m) && (Col < n)) {
+
+        d_Pout[Row*n+Col] = 2*d_Pin[Row*n+Col];
+
+    }
+}
+
 int main() {
     return 0;
 }
